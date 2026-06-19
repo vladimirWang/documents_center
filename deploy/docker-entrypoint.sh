6400a17e47d39a5d25751fff118d4efed2155b41
fernando <413114463@qq.com>
2026-06-19 19:39:18 +0800

    修改entrypoint

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 set -e
 
-DB_HOST="${DB_HOST:-postgres}"
-DB_USER="${DB_USER:-postgres}"
-DB_PASSWORD="${DB_PASSWORD:-postgres}"
-DB_NAME="${DB_NAME:-documents_center}"
-
-echo "ensuring database ${DB_NAME} exists..."
-uv run python - <<PY
-import psycopg
-
-conn = psycopg.connect(
-    host="${DB_HOST}",
-    user="${DB_USER}",
-    password="${DB_PASSWORD}",
-    dbname="postgres",
-)
-conn.autocommit = True
-with conn.cursor() as cur:
-    cur.execute("SELECT 1 FROM pg_database WHERE datname = %s", ("${DB_NAME}",))
-    if cur.fetchone() is None:
-        cur.execute(f'CREATE DATABASE "{DB_NAME}"')
-conn.close()
-PY
 
 echo "running alembic migrations..."
 uv run alembic upgrade head
